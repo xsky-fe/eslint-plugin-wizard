fe58363123b2e237efb504adb65831076e8aeba6
yanzhen <yanzhen@xsky.com>
2017-08-02 16:39:52 +0800

    implement rule: 'use t function'

--- tests/lib/rules/use-t-function.js
+++ tests/lib/rules/use-t-function.js
@@ -8,32 +8,53 @@
 // Requirements
 //------------------------------------------------------------------------------
 
-var rule = require('../../../lib/rules/use-t-function'),
+const rule = require('../../../lib/rules/use-t-function');
+const RuleTester = require('eslint').RuleTester;
 
-  RuleTester = require('eslint').RuleTester;
-
+const parserOptions = {
+  ecmaVersion: 8,
+  sourceType: 'module',
+  ecmaFeatures: {
+    experimentalObjectRestSpread: true,
+    jsx: true
+  }
+};
 
 //------------------------------------------------------------------------------
 // Tests
 //------------------------------------------------------------------------------
 
-var ruleTester = new RuleTester();
+const ruleTester = new RuleTester({ parserOptions });
 ruleTester.run('use-t-function', rule, {
 
   valid: [
-    {
-      code: 'fn()',
-    }
-    // give me some code that won't trigger a warning
+    { code: 'fn()' },
+    { code: '"This is not a chinese string."' },
+    { code: '\'This is not a chinese string.\'' },
+    { code: '<Col xs={6}>{t(\'名称：\')} {name}</Col>' },
+    { code: 'const s = str => `string is ${str}`;' },
+    { code: '<p>{t(\'当前已选择%d个块存储卷 共%d条信息\', { args: [value.length, totalCount] })}</p>' },
   ],
 
   invalid: [
     {
-      code: '',
-      errors: [{
-        message: 'Fill me in.',
-        type: 'Me too'
-      }]
+      code: '<Col xs={6}>名称：{name}</Col>',
+      errors: [
+        {
+          message: '名称： contains Chinese, use t function to wrap it.',
+          type: 'Literal'
+        }
+      ],
+    },
+    {
+      code: '\'。\'',
+      errors: [
+        {
+          message: '。 contains Chinese, use t function to wrap it.',
+          type: 'Literal'
+        }
+      ],
     }
-  ]
+  ],
+
 });
