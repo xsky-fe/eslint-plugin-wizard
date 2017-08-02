7c56e27b7582a2ee4501282ddbfe218cd3ceb272
yanzhen <yanzhen@xsky.com>
2017-08-02 18:30:58 +0800

    iterator plus operator

--- tests/lib/rules/use-t-function.js
+++ tests/lib/rules/use-t-function.js
@@ -34,7 +34,8 @@ ruleTester.run('use-t-function', rule, {
     { code: 'const s = str => `string is ${str}`;' },
     { code: '<Col xs={6}>{t(\'名称：\')} {name}</Col>' },
     { code: '<p>{t(\'当前已选择%d个块存储卷 共%d条信息\', { args: [value.length, totalCount] })}</p>' },
-    { code: '<Well>{t(\'提示：请填写集群初始化密码，确保由管理员安全安装集群，\' + \'初始化密码已经在安装服务器的以下位置生成。\')}</Well>' }
+    { code: '<Well>{t(\'提示：请填写集群初始化密码，确保由管理员安全安装集群，\' + \'初始化密码已经在安装服务器的以下位置生成。\')}</Well>' },
+    { code: 't(\'一\' + \'二\' + \'三\')' },
   ],
 
   invalid: [
@@ -52,6 +53,23 @@ ruleTester.run('use-t-function', rule, {
       errors: [
         {
           message: '。 contains Chinese, use t function to wrap it.',
+          type: 'Literal'
+        }
+      ],
+    },
+    {
+      code: '\'一\' + \'二\' + \'三\'',
+      errors: [
+        {
+          message: '一 contains Chinese, use t function to wrap it.',
+          type: 'Literal'
+        },
+        {
+          message: '二 contains Chinese, use t function to wrap it.',
+          type: 'Literal'
+        },
+        {
+          message: '三 contains Chinese, use t function to wrap it.',
           type: 'Literal'
         }
       ],
